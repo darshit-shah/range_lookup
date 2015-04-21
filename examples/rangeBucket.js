1ad5cf4d9385e0114e6f578194439a8a33a9889e
darshit-shah <darshit@yuktianalytics.com>
2015-04-21 11:48:36 +0530

    corrected example

--- examples/rangeBucket.js
+++ examples/rangeBucket.js
@@ -1,20 +1,21 @@
 var range_lookup = require("../range_lookup");
 
 function test() {
-    range_lookup.addRule("300", '150 - 300', true);
-    range_lookup.addRule(150, '<=150', false);
-    range_lookup.addRule(450, '300 - 450', false);
-    range_lookup.addRule(450, '450', true);
-    range_lookup.defaultName = '>450';
+    var myLookup = new range_lookup();
+    myLookup.addRule("300", '150 - 300', true);
+    myLookup.addRule(150, '<=150', false);
+    myLookup.addRule(450, '300 - 450', false);
+    myLookup.addRule(450, '450', true);
+    myLookup.defaultName = '>450';
 
-    range_lookup.print();
+    myLookup.print();
 
     var values = [-5, 149, 150, 151, 299, 300, 301, 449, 450, 451];
     for (var i = 0; i < 0; i++) {
         values.push(parseInt(Math.random() * 500));
     }
     for (var i = 0; i < values.length; i++) {
-        console.log("Number: " + values[i] + ", Bucket: " + range_lookup.getRange(values[i]));
+        console.log("Number: " + values[i] + ", Bucket: " + myLookup.getRange(values[i]));
     }
 }
 
